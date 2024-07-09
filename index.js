772e31051f633ad7d23238b45a6e2b649f28a54c
Beserker-356 <jenish.23bcs10016@ms.sst.scaler.com>
2024-07-09 18:33:10 +0530

    Added Signup and Signin

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const path = require("path");
+const userRoute = require("./routes/user");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/blogme")
+  .then((e) => console.log("MongoDB connected"));
 
 const app = express();
 const PORT = 8000;
 
+app.use(express.urlencoded({extended: false}))
+
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.use("/user", userRoute);
 
 app.listen(PORT, () => console.log(`Server started at port: ${PORT}`));
