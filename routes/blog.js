862adb2a121968e9a0e1abefc7ff923d5a799376
Beserker-356 <jenish.23bcs10016@ms.sst.scaler.com>
2024-07-11 19:18:39 +0530

    Added CRUD for comment model

--- routes/blog.js
+++ routes/blog.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const multer = require("multer");
 const path = require("path");
 const Blog = require("../models/blog");
+const Comment = require("../models/comment");
 
 const router = Router();
 
@@ -25,11 +26,24 @@ router.get("/add-new", (req, res) => {
 
 router.get("/:id", async (req, res) => {
   const blog = await Blog.findById(req.params.id).populate("createdBy");
-  console.log(blog);
+  const comments = await Comment.find({ blogId: req.params.id }).populate(
+    "createdBy"
+  );
+  console.log(comments);
   return res.render("blog", {
     user: req.user,
     blog,
+    comments,
   });
+});
+
+router.post("/comment/:blogId", async (req, res) => {
+  await Comment.create({
+    content: req.body.comment,
+    blogId: req.params.blogId,
+    createdBy: req.user._id,
+  });
+  return res.redirect(`/blog/${req.params.blogId}`);
 });
 
 router.post("/", upload.single("coverImage"), async (req, res) => {
